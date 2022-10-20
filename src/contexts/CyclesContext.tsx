abb48b9cf3810e1fdcd5a64117b6e987c17ce531
Misael Lopes <mecl.ely@gmail.com>
2022-10-20 16:09:42 +0100

    Project 2 Completed

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,20 +1,15 @@
-import { createContext, ReactNode, useState } from "react";
+import { differenceInSeconds } from "date-fns";
+import { createContext, ReactNode, useEffect, useReducer, useState } from "react";
+import { ActionTypes, addNewCycleAction, interruptCurrentCycleAction, setCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
 
 interface CreateCycleData {
     task: string
     minutesDuration: number
 }
 
-interface Cycle {
-    id: string
-    task: string
-    minutesDuration: number
-    startDate: Date
-    interruptedDate?: Date
-    finishedDate?: Date
-}
-
 interface CyclesContextType {
+    cycles: Cycle[]
     activeCycle: Cycle | undefined
     activeCycleId: string | null
     setCurrentCycleAsFinished: () => void
@@ -32,24 +27,53 @@ interface CyclesContextProviderProps {
 
 export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
 
-    const [cycles, setCycles] = useState<Cycle[]>([])
-    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-    const [totalSecondsPassed, setTotalSecondsPassed] = useState(0)
+    const [cycleState, dispatch] = useReducer(cyclesReducer, {
+        cycles: [],
+        activeCycleId: null
+    }, () => {
+        const storedStateAsJSON = localStorage.getItem('@ignite_timer:cycle_state(1.0.0)')
 
+        if (storedStateAsJSON) {
+            return JSON.parse(storedStateAsJSON)
+        }
+
+        return {
+            cycles: [],
+            activeCycleId: null
+        }
+    })
+
+    const { cycles, activeCycleId } = cycleState
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+    const [totalSecondsPassed, setTotalSecondsPassed] = useState(() => {
+
+        if (activeCycle) {
+            return differenceInSeconds(
+                new Date(),
+                new Date(activeCycle.startDate)
+            )
+        }
+
+        return 0
+    })
+
+    useEffect(() => {
+        const stateJSON = JSON.stringify(cycleState)
+        // Recommended: Save with specific name and put app version to prevent bugs through different app versions
+        localStorage.setItem('@ignite_timer:cycle_state(1.0.0)', stateJSON)
+    }, [cycleState])
+
+
+
+
 
     function setSecondsPassed(seconds: number) {
         setTotalSecondsPassed(seconds)
     }
 
     function setCurrentCycleAsFinished() {
-        setCycles((state) => state.map(cycle => {
-            if (cycle.id === activeCycleId) {
-                return { ...cycle, finishedDate: new Date() }
-            } else {
-                return cycle
-            }
-        }))
+        dispatch(setCurrentCycleAsFinishedAction())
     }
 
     function createNewCycle(data: CreateCycleData) {
@@ -60,29 +84,17 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
             minutesDuration: data.minutesDuration,
             startDate: new Date()
         }
-        setCycles(state => [...state, newCycle])
-        // always when a state data needs to be updated, is a common practice
-        // to use a arrow function to copy the actual data and add the new value at the end
-        setActiveCycleId(id)
+        dispatch(addNewCycleAction(newCycle))
         setTotalSecondsPassed(0)
-
-        // reset()
     }
 
     function interruptCurrentCycle() {
-        setCycles((state) => state.map(cycle => {
-            if (cycle.id === activeCycleId) {
-                return { ...cycle, interruptedDate: new Date() }
-            } else {
-                return cycle
-            }
-        }))
-
-        setActiveCycleId(null)
+        dispatch(interruptCurrentCycleAction())
     }
 
     return (
         <CyclesContext.Provider value={{
+            cycles,
             activeCycle,
             activeCycleId,
             setCurrentCycleAsFinished,
