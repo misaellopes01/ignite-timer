c713370fa18ffe28e5eed8f85f2a2143a932b96f
Misael Lopes <mecl.ely@gmail.com>
2022-12-29 15:04:46 +0100

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
-import { Baby, BabyProps } from './components/Baby/Index'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
-const babyProps: BabyProps = {
-    name: 'Dawith Arthur',
-    codeName: 'Steve Jr',
-}
+// const babyProps: BabyProps = {
+//     name: 'Dawith Arthur',
+//     codeName: 'Steve Jr',
+// }
 
 export function Router() {
     return (
@@ -16,7 +15,7 @@ export function Router() {
                 <Route path='/' element={<Home />} />
                 <Route path='/history' element={<History />} />
             </Route>
-            {/* <Route path='/baby' element={<Baby key={1} codeName={babyProps.codeName} name={babyProps.name} />} /> */}
+           
         </Routes>
     )
 }
